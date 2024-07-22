e8e311801bc183bce4e3c76127606153066c6fe0
mubeenaftab <mubeenaftab66@gmail.com>
2024-07-22 13:08:05 +0500

    removed frontend from backend

--- app1_mubeen/REACT/react/src/api.js
+++
@@ -1,9 +0,0 @@
-// api connect library axios to connect react and fastapi
-import axios from 'axios'
-
-
-const api = axios.create({
-    baseURL: 'http://localhost:8000',
-});
-
-export default api;
